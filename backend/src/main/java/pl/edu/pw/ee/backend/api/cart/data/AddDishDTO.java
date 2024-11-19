e2242d178aabb64cafc673f10a140734f87732ac
Szymon Półtorak <szymonpotorak@gmail.com>
2024-11-19 10:58:03 +0100

    Fixed dto

--- backend/src/main/java/pl/edu/pw/ee/backend/api/cart/data/AddDishDTO.java
+++ backend/src/main/java/pl/edu/pw/ee/backend/api/cart/data/AddDishDTO.java
@@ -2,10 +2,11 @@ package pl.edu.pw.ee.backend.api.cart.data;
 
 import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
+import pl.edu.pw.ee.backend.entities.dish.MealType;
 
 import java.util.List;
 
 @Builder
-public record AddDishDTO(int calories, int cateringCompanyId, List<String> ingredients, String kitchenType, String mealType,
+public record AddDishDTO(int calories, int cateringCompanyId, List<String> ingredients, String kitchenType, MealType mealType,
                          String name, MultipartFile photo, float price) {
 }
