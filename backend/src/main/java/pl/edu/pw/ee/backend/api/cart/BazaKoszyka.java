ee4abfd9e3598679761d836b3d2bb828bdc0f78e
Szymon Półtorak <szymonpotorak@gmail.com>
2024-11-20 18:01:12 +0100

    Fixed naming

--- backend/src/main/java/pl/edu/pw/ee/backend/api/cart/BazaKoszyka.java
+++ backend/src/main/java/pl/edu/pw/ee/backend/api/cart/BazaKoszyka.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.edu.pw.ee.backend.api.cart.interfaces.IKoszykAPI;
+import pl.edu.pw.ee.backend.api.cart.data.FindDishDTO;
+import pl.edu.pw.ee.backend.api.cart.interfaces.IBazaKoszyka;
 import pl.edu.pw.ee.backend.entities.cart.Cart;
 import pl.edu.pw.ee.backend.entities.cart.CartRepository;
 import pl.edu.pw.ee.backend.entities.dish.Dish;
@@ -15,9 +16,14 @@ import pl.edu.pw.ee.backend.utils.exceptions.cart.CartNotFoundException;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class KoszykService implements IKoszykAPI {
+public class BazaKoszyka implements IBazaKoszyka {
     private final CartRepository cartRepository;
     private final DishRepository dishRepository;
+
+    @Override
+    public FindDishDTO getCart(int cartId) {
+        return null;
+    }
 
     @Override
     @Transactional
