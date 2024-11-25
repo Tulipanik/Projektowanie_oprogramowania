dafd05df260da44442520042d2e4cde7651cec3c
Szymon Półtorak <szymonpotorak@gmail.com>
2024-11-25 21:49:59 +0100

    Added parameter for max compatibility

--- backend/src/main/java/pl/edu/pw/ee/backend/entities/order/OrderRepository.java
+++ backend/src/main/java/pl/edu/pw/ee/backend/entities/order/OrderRepository.java
@@ -2,6 +2,7 @@ package pl.edu.pw.ee.backend.entities.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,6 +10,6 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     @Query("SELECT o FROM Order o WHERE o.orderData.client.clientId = :clientId")
-    List<Order> findOrdersByClientId(int clientId);
+    List<Order> findOrdersByClientId(@Param("clientId") int clientId);
 
 }
