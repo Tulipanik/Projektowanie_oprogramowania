14d4ff23aaa4825a2ef8991c68390203ca3d6b10
MilczarekJan <01169598@pw.edu.pl>
2024-11-28 20:25:08 +0100

    dodanie managera

---
+++ backend/src/main/java/pl/edu/pw/ee/backend/api/company/ManagerDanychFirmZewnetrznych.java
@@ -0,0 +1,18 @@
+package pl.edu.pw.ee.backend.api.company;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.edu.pw.ee.backend.api.auth.data.ExternalCompanyCreateDTO;
+import pl.edu.pw.ee.backend.api.company.interfaces.IBazaDanychFirmyZewnetrznej;
+import pl.edu.pw.ee.backend.api.company.interfaces.IFirmaZewnetrznaAPI;
+
+@Component
+@RequiredArgsConstructor
+public class ManagerDanychFirmZewnetrznych implements IFirmaZewnetrznaAPI {
+    private final IBazaDanychFirmyZewnetrznej bazaDanychFirmyZewnetrznej;
+
+    @Override
+    public int addNewExternalCompany(ExternalCompanyCreateDTO companyData) {
+        return bazaDanychFirmyZewnetrznej.addCateringCompanyData(companyData);
+    }
+}
