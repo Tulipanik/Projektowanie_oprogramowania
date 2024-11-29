6bfef538b685158b67670664ba23c784ce950ab4
Szymon Półtorak <szymonpotorak@gmail.com>
2024-11-29 14:39:43 +0100

    Updated service class

--- backend/src/main/java/pl/edu/pw/ee/backend/api/company/BazaDanychFirmyZewnetrznej.java
+++ backend/src/main/java/pl/edu/pw/ee/backend/api/company/BazaDanychFirmyZewnetrznej.java
@@ -5,11 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import pl.edu.pw.ee.backend.api.auth.data.AccountCreateDTO;
 import pl.edu.pw.ee.backend.api.auth.data.ExternalCompanyCreateDTO;
 import pl.edu.pw.ee.backend.api.company.interfaces.IBazaDanychFirmyZewnetrznej;
+import pl.edu.pw.ee.backend.api.company.interfaces.IExternalCompanyService;
 import pl.edu.pw.ee.backend.entities.external.company.ExternalCompany;
-import pl.edu.pw.ee.backend.entities.external.company.ExternalCompanyRepository;
 import pl.edu.pw.ee.backend.entities.user.Role;
 import pl.edu.pw.ee.backend.entities.user.User;
 
@@ -17,7 +16,7 @@ import pl.edu.pw.ee.backend.entities.user.User;
 @RequiredArgsConstructor
 @Slf4j
 public class BazaDanychFirmyZewnetrznej implements IBazaDanychFirmyZewnetrznej {
-    private final ExternalCompanyRepository externalCompanyRepository;
+    private final IExternalCompanyService externalService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -25,16 +24,9 @@ public class BazaDanychFirmyZewnetrznej implements IBazaDanychFirmyZewnetrznej {
     public int addCateringCompanyData(ExternalCompanyCreateDTO request) {
         log.info("Creating catering company account : {}", request);
 
-        ExternalCompany externalCompany = ExternalCompany
-                .builder()
-                .address(request.address())
-                .NIP(request.NIP())
-                .companyType(request.companyType())
-                .phoneNumber(request.phoneNumber())
-                .user(buildUserFromRequest(request, Role.EXTERNAL_COMPANY))
-                .build();
+        ExternalCompany externalCompany = buildExternalCompanyFromRequest(request);
 
-        externalCompany = externalCompanyRepository.save(externalCompany);
+        externalCompany = externalService.save(externalCompany);
 
         log.info("Catering company account created : {}", externalCompany);
 
@@ -45,8 +37,15 @@ public class BazaDanychFirmyZewnetrznej implements IBazaDanychFirmyZewnetrznej {
         return buildUserFromRequest(request.name(), request.username(), request.password(), role);
     }
 
-    private User buildUserFromRequest(AccountCreateDTO request, Role role) {
-        return buildUserFromRequest(request.name(), request.username(), request.password(), role);
+    private ExternalCompany buildExternalCompanyFromRequest(ExternalCompanyCreateDTO request) {
+        return ExternalCompany
+                .builder()
+                .address(request.address())
+                .NIP(request.NIP())
+                .companyType(request.companyType())
+                .phoneNumber(request.phoneNumber())
+                .user(buildUserFromRequest(request, Role.EXTERNAL_COMPANY))
+                .build();
     }
 
     private User buildUserFromRequest(String name, String username, String password, Role role) {
