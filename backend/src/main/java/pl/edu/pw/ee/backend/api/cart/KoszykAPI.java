f50ea50d87a2bde5d5779c45619ea46be450950b
Szymon Półtorak <szymonpotorak@gmail.com>
2024-12-04 20:15:41 +0100

    Added removing cart method

--- backend/src/main/java/pl/edu/pw/ee/backend/api/cart/KoszykAPI.java
+++ backend/src/main/java/pl/edu/pw/ee/backend/api/cart/KoszykAPI.java
@@ -3,7 +3,12 @@ package pl.edu.pw.ee.backend.api.cart;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pw.ee.backend.api.cart.data.FindDishDTO;
 import pl.edu.pw.ee.backend.api.cart.interfaces.IKoszykAPI;
 
@@ -19,13 +24,21 @@ import java.util.List;
 public class KoszykAPI implements IKoszykAPI {
     private final IKoszykAPI koszykAPI;
 
+    @Override
     @PostMapping("/{cartId}/dishes/{dishId}")
     public void addDishToCart(@PathVariable int cartId, @PathVariable int dishId) {
         koszykAPI.addDishToCart(cartId, dishId);
     }
 
+    @Override
     @GetMapping("/{cartId}")
     public List<FindDishDTO> getCart(@PathVariable int cartId) {
         return koszykAPI.getCart(cartId);
+    }
+
+    @Override
+    @DeleteMapping("/{cartId}/dishes/{dishId}")
+    public void removeDishFromCart(@PathVariable int cartId, @PathVariable int dishId) {
+        koszykAPI.removeDishFromCart(cartId, dishId);
     }
 }
